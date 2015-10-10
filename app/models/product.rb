a086ddae9b04cb7b62691515014f078805bd1b96
Taylor <taylor_funk@live.com>
2015-10-10 15:34:54 -0600

    Add title to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
 
      validates :image, presence: true   
 validates :description, presence: true
+validates :title, presence: true
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010211738) do
+ActiveRecord::Schema.define(version: 20151010212914) do
 
   create_table "products", force: :cascade do |t|
     t.string   "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151010211738) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "name"
+    t.string   "title"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
