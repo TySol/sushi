8b46208d262b71428d8cae1ab4336b19d86bbad3
Taylor <taylor_funk@live.com>
2015-10-10 14:27:17 -0600

    Fixed paperclip.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'http://sushione.herokuapp.com/' }
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
-
 end
