ddc13637766765c0b7e67c4003bc1dfb60cd45fa
Taylor <taylor_funk@live.com>
2015-10-10 15:08:08 -0600

    Image size

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 	belongs_to :user 
 	
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600x600" }
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
      validates :image, presence: true   
 validates :description, presence: true
