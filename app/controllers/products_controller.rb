4cf53b45c4831a916c20265763ca16ab0f367308
Taylor <taylor_funk@live.com>
2015-10-23 21:36:20 -0600

    Dev pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all.order(:title).paginate(:page => params[:page], :per_page => 48)
+    @products = Product.all.order(:title).paginate(:page => params[:page], :per_page => 8)
   end
 
   # GET /products/1
